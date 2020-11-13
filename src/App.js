41e41c74396f0cebc23e51502bbd43d5598f6348
tautme <hughes@pobox.com>
2020-11-12 22:00:27 -0600

    first commit text change in pp.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let us build an image shelf and money bucket.
         </p>
         <a
           className="App-link"
